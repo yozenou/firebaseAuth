d1abfbaeda7cfc506c86036fea3215ac31b3a7a8
Yohan Zenou <yohan@gagglon.com>
2018-02-27 19:27:44 +0200

    First commit

---
+++ app/config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = "AIzaSyDNo-KwwstIEeDdvNXdGtAJ_OaZetCZNGI";
+export const FIREBASE_AUTH_DOMAIN = "first-project-abfed.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https://first-project-abfed.firebaseio.com";
+export const FIREBASE_PROJECT_ID = "first-project-abfed";
+export const FIREBASE_STORAGE_BUCKET = "first-project-abfed.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "712316802936";
+
+export const FACEBOOK_APP_ID = "422722731491258";
